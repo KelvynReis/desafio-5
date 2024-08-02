2a50abe158def555864601cafb0f7bd53f62f1cc
Marcew44 <120995295+Marcew44@users.noreply.github.com>
2024-08-02 20:02:03 -0300

    Update da frase do alert no script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Digite alguma coisa");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
